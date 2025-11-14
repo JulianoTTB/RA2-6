6a3ce66d61c2107bb6132a995092ca65d655237a
GeandroMarques <166722959+GeandroMarques@users.noreply.github.com>
2025-11-14 13:24:48 -0300

    Update Sistema.hs
    Criação de todos os tipos de dados e a função de remover item

--- Sistema.hs
+++ Sistema.hs
@@ -1,1 +1,51 @@
+import qualified Data.Map as Map
+import Data.Time (UTCTime)
 
+data Item = Item {
+    itemID :: String,
+    nome :: String,
+    quantidade :: Int,
+    categoria :: String
+} deriving (Show, Read)
+
+data Inventario = Inventario {
+    itens :: Map.Map String Item
+} deriving (Show, Read)
+
+data AcaoLog = Add | Remove | Update | QueryFail
+    deriving (Show, Read)
+
+data StatusLog = Sucesso | Falha String
+    deriving (Show, Read)
+
+data LogEntry = LogEntry {
+    timestamp :: UTCTime,
+    acao :: AcaoLog,
+    detalhes :: String,
+    status :: StatusLog
+} deriving (Show, Read)
+
+type ResultadoOperacao = (Inventario, LogEntry)
+
+removeItem :: UTCTime -> Item -> Inventario -> Either String ResultadoOperacao
+removeItem time itemRemovido (Inventario mapa) =
+    case Map.lookup (itemID itemRemovido) mapa of
+        Just _ ->
+            let novoMapa = Map.delete (itemID itemRemovido) mapa
+                novoInventario = Inventario novoMapa
+                logEntry = LogEntry
+                    { timestamp = time
+                    , acao = Remove
+                    , detalhes = "Item removido: " ++ itemID itemRemovido
+                    , status = Sucesso
+                    }
+            in Right (novoInventario, logEntry)
+
+        Nothing ->
+            let logEntry = LogEntry
+                    { timestamp = time
+                    , acao = Remove
+                    , detalhes = "Falha ao remover. O item não existe: " ++ itemID itemRemovido
+                    , status = Falha "O item não existe"
+                    }
+            in Left "O item não existe"
