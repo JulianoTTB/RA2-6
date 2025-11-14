b22d9fff01c5ab916420cb219326e6e83625f0cf
Alissinnho <166721852+Alissinnho@users.noreply.github.com>
2025-11-14 20:13:46 -0300

    Update Sistema.hs

--- Sistema.hs
+++ Sistema.hs
@@ -75,6 +75,60 @@ addItem time itemAdicionado (Inventario mapa) =
                     }
             in Right (novoInventario, logEntry)
 
+updateQty :: UTCTime -> String -> Int -> Inventario -> Either LogEntry ResultadoOperacao
+updateQty time itemID quantidadepAtualizar (Inventario mapa) =
+    case Map.lookup itemID mapa of
+        Just itemAtual ->
+            let novaQuantidade = quantidade itemAtual + quantidadepAtualizar
+            in if novaQuantidade < 0
+                then
+                    let logEntry = LogEntry
+                            { timestamp = time
+                            , acao = Update
+                            , detalhes = "Falha ao atualizar, o estoque ficou negativo, item: " ++ itemID
+                            , status = Falha "estoque negativo"
+                            }
+                    in Left "estoque negativo"
+
+                else
+                    let itemAtualizado = itemAtual { quantidade = novaQuantidade }
+                        novoMapa = Map.insert itemID itemAtualizado mapa
+                        novoInventario = Inventario novoMapa
+                        logEntry = LogEntry
+                            { timestamp = time
+                            , acao = Update
+                            , detalhes = "Quantidade atualizada! item: " ++ itemID
+                            , status = Sucesso
+                            }
+                    in Right (novoInventario, logEntry)
+
+        Nothing ->
+            let logEntry = LogEntry
+                    { timestamp = time
+                    , acao = Update
+                    , detalhes = "Falha ao atualizar, o item não existe, item: " ++ itemID
+                    , status = Falha "Item não encontrado"
+                    }
+            in Left "item não existe"
+
+logsDeErro :: [LogEntry] -> [LogEntry]
+logsDeErro = filter erroExiste
+  where
+    erroExiste (LogEntry _ _ _ (Falha _)) = True
+    erroExiste _ = False
+
+report :: [LogEntry] -> String
+report logs =
+    let erros = logsDeErro(logs)
+        totalErros = length erros
+        totalLogs = length logs
+    in
+        "-===RELATÓRIO===-\n\n" ++
+        "Total de operações: " ++ show totalLogs ++ "\n" ++
+        "Total de erros: " ++ show totalErros ++ "\n" ++
+        "Erros registrados:\n" ++
+        unlines (map formatarLog erros)
+
 
 
 historicoPorItem :: String -> [LogEntry] -> [LogEntry]
